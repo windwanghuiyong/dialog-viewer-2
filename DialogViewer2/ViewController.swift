77398ce3740a1e39d7bf221b589c3bcb83000958
王慧勇 <weifeng19911024@163.com>
2017-01-26 10:21:45 +0800

    Initial Commit

---
+++ DialogViewer2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DialogViewer2
+//
+//  Created by wanghuiyong on 26/01/2017.
+//  Copyright © 2017 Personal Organization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
